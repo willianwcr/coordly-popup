bbaae195cd917819486a6416c5f3005d7f649850
Willian Rodrigues <will@wcr.design>
2024-03-21 13:32:45 +0000

    Eddited main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var PopupCoordly_tid = setInterval( function () {
     if ( document.readyState !== 'complete' ) return;
     clearInterval( PopupCoordly_tid );       
     var __proprofs_version= Math.floor((Math.random() * 9999999) + 1)+ Math.floor(Date.now() / 1000);
-    var popupCoordlyBaseUrl = 'raw.githubusercontent.com/willianwcr/coordly-popup/main/popup.js';
+    var popupCoordlyBaseUrl = 'willianwcr.github.io/coordly-popup/popup.js';
     if(document.querySelectorAll('script[src*="' + popupCoordlyBaseUrl + '"]').length == 0){
         var script = document.createElement("script"); 
         script.src="https://" + popupCoordlyBaseUrl + "?v="+ +__proprofs_version;
